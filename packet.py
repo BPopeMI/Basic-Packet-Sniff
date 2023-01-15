f0f770aae88c5809e1eb4ba37924f8a6759278d2
Green <Green>
2023-01-15 16:03:37 -0500

    Packet.py
    Basic Packet Sniffer
    Exports collected information into a html table.

--- packet.py
+++ packet.py
@@ -1,38 +1,53 @@
-import os
-import datetime
 from scapy.all import *
+import os
+from datetime import datetime
 
-# create a new file on the desktop
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
-file_name = "sniffed_packets.txt"
-file_path = os.path.join(desktop, file_name)
-with open(file_path, "w") as f:
-    f.write("Sniffed packets:\n\n")
+# Set a color for each type of packet
+colors = {
+    "IPv4": "green",
+    "ARP": "red",
+    "IPv6": "blue"
+}
 
-stop_sniffing = False
+# Create an HTML file to store the output
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+file_path = os.path.join(desktop, "output.html")
+with open(file_path, "w") as file:
+    file.write("<html>\n<head>\n")
+    file.write("<style>\n")
+    file.write("table, th, td {\n")
+    file.write("border: 1px solid black;\n")
+    file.write("border-collapse: collapse;\n")
+    file.write("padding: 5px;\n")
+    file.write("}\n")
+    file.write("th {\n")
+    file.write("background-color: lightgray;\n")
+    file.write("}\n")
+    file.write("</style>\n")
+    file.write("</head>\n<body>\n")
+    file.write("<table>\n")
+    file.write("<tr><th>Timestamp</th><th>Source MAC</th><th>Source IP</th><th>Destination MAC</th><th>Destination IP</th><th>Protocol</th></tr>\n")
+    def packet_info(packet):
+        src_mac = packet[Ether].src
+        dst_mac = packet[Ether].dst
+        if packet.haslayer(IP):
+            protocol = "IPv4"
+            src_ip = packet[IP].src
+            dst_ip = packet[IP].dst
+        elif packet.haslayer(ARP):
+            protocol = "ARP"
+            src_ip = packet[ARP].psrc
+            dst_ip = packet[ARP].pdst
+        elif packet.haslayer(IPv6):
+            protocol = "IPv6"
+            src_ip = packet[IPv6].src
+            dst_ip = packet[IPv6].dst
+        else:
+            src_ip = "Unknown"
+            dst_ip = "Unknown"
+            protocol = "Unknown"
 
-def handle_pause_resume(packet):
-    global stop_sniffing
-    if stop_sniffing:
-        return
-    packet_str = "Timestamp: {}\nSource MAC: {}\nDestination MAC: {}\nSource IP: {}\nDestination IP: {}\nPacket size: {}\nProtocol: {}\n\n".format(datetime.datetime.now(), packet[Ether].src, packet[Ether].dst, packet[IP].src, packet[IP].dst, packet[IP].len, packet[IP].proto)
-    with open(file_path, "a") as f:
-        f.write(packet_str)
-    print(packet_str)
-    
-def start_sniffing():
-    global stop_sniffing
-    stop_sniffing = False
-    sniff(prn=handle_pause_resume, filter="ip", store=0)
-    
-def stop_sniffing():
-    global stop_sniffing
-    stop_sniffing = True
+        file.write("<tr><td>" + str(datetime.now()) + "</td><td style='color: " + colors.get(protocol, "black") + "'>" + src_mac + "</td><td>" + src_ip + "</td><td>" + dst_mac + "</td><td>" + dst_ip + "</td><td>" + protocol + "</td></tr>\n")
 
-# start sniffing
-start_sniffing()
-
-# code to handle pause and resume
-while True:
-        user_input = input("Enter 'p' to pause, 'r' to resume, 'q' to quit: ")
-
+    sniff(prn=packet_info)
+    file.write("</table>\n</body>\n</html>")
